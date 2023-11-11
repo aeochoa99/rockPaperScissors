205fc3f4f8e7fe521073264b3720c57c270bea6f
aeochoa99 <aeochoa99@gmail.com>
2023-11-11 13:55:07 -0800

    Add a function getComputerChoice
    This function will generate a random choice for the cpu

---
+++ script.js
@@ -0,0 +1,5 @@
+function getComputerChoice() {
+    const choices = ['rock', 'paper', 'scissors'];
+    let computerChoice = choices[Math.floor(Math.random() * choices.length)];
+    return computerChoice;
+}
