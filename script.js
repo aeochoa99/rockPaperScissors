7c7fe3d8c6d6e9c8e3d82080c4b89e8132047b6a
aeochoa99 <aeochoa99@gmail.com>
2023-11-11 14:44:27 -0800

    Add a function playRound
    This function plays a round of the game and returns the results
    User input is made case insenstive
    If user input is null returns a string

--- script.js
+++ script.js
@@ -3,3 +3,24 @@ function getComputerChoice() {
     let computerChoice = choices[Math.floor(Math.random() * choices.length)];
     return computerChoice;
 }
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = prompt("Make your choice");
+    if (playerSelection !== null) {
+        playerSelection = playerSelection.toLowerCase();
+    } else {
+        return "Maybe another time."
+    }
+    computerSelection = getComputerChoice();
+    if (playerSelection == 'rock' && computerSelection == 'scissors' ||
+        playerSelection == 'paper' && computerSelection == 'rock' ||
+        playerSelection == 'scissors' && computerSelection == 'paper') {
+            return `You win! ${playerSelection} beats ${computerSelection}!`;
+        } else if (computerSelection == 'rock' && playerSelection == 'scissors' ||
+                   computerSelection == 'paper' && playerSelection == 'rock' ||
+                   computerSelection == 'scissors' && playerSelection == 'paper') {
+                    return `You lose! ${computerSelection} beats ${playerSelection}`;
+                   } else {
+                    return `It's a tie! You both selected ${playerSelection}`;
+                   }
+}
